4fec20b7d4767b735f0ef5fe86730bba3223ab9a
Barth <carlosbarth2019@gmail.com>
2022-04-05 20:27:21 -0300

    ControllerConexao

---
+++ T1Socket/src/Interfaces/InterfaceSocketConnection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Barth
+ */
+public abstract class InterfaceSocketConnection {
+    
+    public static void createConnection()  throws IOException,ParseException {};
+}
