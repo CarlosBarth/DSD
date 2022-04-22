1640d457c02358ec394f0b446a29f240b42c7c6f
Barth <carlosbarth2019@gmail.com>
2022-04-21 21:52:02 -0300

    Ajustes

--- T1Socket/src/app/T1Socket.java
+++ T1Socket/src/app/T1Socket.java
@@ -5,39 +5,17 @@
  */
 package app;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 /**
  *
- * @author 07166848960
+ * @author Barth
  */
 public class T1Socket {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         
-        System.out.println("Criando Conexão...");
-        try (Socket conn =  new Socket("10.60.185.67",80)) {
-            conn.setReuseAddress(true);
-            System.out.println("Conectado!");
-            InputStream in = conn.getInputStream();
-            
-            byte [] dados = new byte[1024];
-            int qtde = in.read(dados);
-            
-            while (qtde >= 0) {
-                String dadosStr = new String(dados, 0, qtde);
-                System.out.println(dadosStr);
-                qtde = in.read(dados);
-            }
-        } catch (UnknownHostException e ) {
-            System.out.println("Host não Encontrado");
-            e.printStackTrace();
-        }
+        Controller.ControllerConexao.main(args);
     }
 }
